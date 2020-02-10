04f9057888b3fef08b43bf20bc6443ae6494a891
zacarias <zac23or@gmail.com>
2020-02-10 19:36:40 -0300

    Revert "Require in correct line"
    This reverts commit 621c9bac6723ee98551672da422b1be8c4cc98d8.

--- web-launcher.lisp
+++ web-launcher.lisp
@@ -1,9 +1,10 @@
 (require :asdf)
+(require :woo)
 
 (asdf:disable-output-translations)
 (defvar *port* (parse-integer (asdf::getenv "PORT")))
 (load "root/quicklisp/setup.lisp")
-(require :woo)
+(require :web-server)
 
 (woo:run
   (lambda (env)
