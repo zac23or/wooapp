76277db7fef2ff956195bb8bed9584d03953af4b
zacarias <zac23or@gmail.com>
2020-02-10 20:04:44 -0300

    load order

--- web-launcher.lisp
+++ web-launcher.lisp
@@ -1,7 +1,7 @@
-(defvar *port* (parse-integer (asdf::getenv "PORT")))
 (load "root/quicklisp/setup.lisp")
 (require :asdf)
 (asdf:disable-output-translations)
+(defvar *port* (parse-integer (asdf::getenv "PORT")))
 (require :woo)
 
 (require :web-server)
