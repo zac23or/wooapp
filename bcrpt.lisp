6679e044ca8d262b8aa0553243f6a092256f1b19
zacarias <zac23or@gmail.com>
2020-02-23 17:01:42 -0300

    bcrpt adicionado

---
+++ bcrpt.lisp
@@ -0,0 +1,20 @@
+
+(defpackage :bcrpt
+  (:use cl)
+  (:local-nicknames (:rds :redis))
+  (:use cl-json)
+  (:use bcrypt)
+  (:export :bcrptresponse)
+  )
+(in-package :bcrpt)
+(defun bcrptresponse()
+    
+  (let ((now (get-internal-real-time)) (text ""))
+    (bcrypt:hash "plain text password")
+    (setq text (write-to-string (- (get-internal-real-time) now)))
+    `(200 (:content-type "text/html") (,text))
+
+    )
+  )
+
+
